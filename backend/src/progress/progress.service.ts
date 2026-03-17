2d367e62c0d32069b387ac1d0c703fcfc2c7a16d
meryemlitim <litimmeryem2001@gmail.com>
2026-03-17 10:58:57 +0000

    QS-26 Auto-create Progress on Register

--- backend/src/progress/progress.service.ts
+++ backend/src/progress/progress.service.ts
@@ -2,9 +2,22 @@ import { Injectable } from "@nestjs/common";
 import { Progress, ProgressDocument } from "./schemas/progress.schema";
 import { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
+import { Step } from "src/common/enums/step.enum";
 
 @Injectable()
 export class ProgressService {
-  constructor(@InjectModel(Progress.name) private ProgressModule: Model<ProgressDocument>) {}
+  constructor(@InjectModel(Progress.name) private progressModel: Model<ProgressDocument>) {}
 
+   async create(userId: string): Promise<ProgressDocument> {
+    const progress = new this.progressModel({
+      userId,
+      currentHizb: 1,
+      currentSorat: 1,
+      currentAya: 0,
+      step: Step.READING,
+      unlockedHizbs: [1],
+      unlockedSorats: [1],
+    });
+    return progress.save();
+  }
 }
