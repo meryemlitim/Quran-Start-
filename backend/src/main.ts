83f3300da84258aa0898adc54afe4de952b2d718
meryemlitim <litimmeryem2001@gmail.com>
2026-03-27 16:46:41 +0100

    fix main in the backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: '*',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
